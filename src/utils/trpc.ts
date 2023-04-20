9858f8f53de64a578480e52291701ff2c111b181
Fiston <60503084+Fiston-pro@users.noreply.github.com>
2023-04-20 16:56:29 +0200

    Added a new client

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -11,10 +11,6 @@ function getBaseUrl() {
     // reference for vercel.com
     return `https://${process.env.VERCEL_URL}`;
 
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-
   // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
